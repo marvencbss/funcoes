5d0deaf6f5c2db781cd91e2b8fdb60d72dccd7a8
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-05-03 21:22:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-// function calcularArea(numAlt,numLar) {
-//     const altura = numAlt;
-//     const largura = numLar;
-//     const area = altura * largura;
-//     return area;
-// }
-// console.log(calcularArea(10,2));
-// console.log(calcularArea(12,90));
+function calcularArea(numAlt,numLar) {
+    const altura = numAlt;
+    const largura = numLar;
+    const area = altura * largura;
+    return area;
+}
+console.log(calcularArea(10,2));
+console.log(calcularArea(12,90));
 
 
 //ex 1
 
-// function imprimeConsole() {
-//     console.log("Olá mundo");
-// }
-// imprimeConsole()
+function imprimeConsole() {
+    console.log("Olá mundo");
+}
+imprimeConsole()
 
 //ex 2
 
-// function imprimeNome(nome) {
-//     console.log(`Olá ${nome}`);
-// }
-// imprimeNome("Maria")
-// imprimeNome("Antonieta")
-// imprimeNome("Felipe")
+function imprimeNome(nome) {
+    console.log(`Olá ${nome}`);
+}
+imprimeNome("Maria")
+imprimeNome("Antonieta")
+imprimeNome("Felipe")
 
-// function colocarnomeMaiusculo(nome){
-//     const nomeMaiusculo = nome.toUpperCase()
-//     return nomeMaiusculo
-// }
+function colocarnomeMaiusculo(nome){
+    const nomeMaiusculo = nome.toUpperCase()
+    return nomeMaiusculo
+}
 
-// console.log(colocarnomeMaiusculo("Maria Antonieta"));
+console.log(colocarnomeMaiusculo("Maria Antonieta"));
 
 function somar(nmr1,nmr2) {
     const soma = nmr1 + nmr2
