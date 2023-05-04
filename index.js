41531cd9d265d719fd2714d40427df83d44796c0
marvencbss <marvencbs12@gmail.com>
2023-05-03 21:19:20 -0300

    funcoes

---
+++ index.js
@@ -0,0 +1,39 @@
+// function calcularArea(numAlt,numLar) {
+//     const altura = numAlt;
+//     const largura = numLar;
+//     const area = altura * largura;
+//     return area;
+// }
+// console.log(calcularArea(10,2));
+// console.log(calcularArea(12,90));
+
+
+//ex 1
+
+// function imprimeConsole() {
+//     console.log("Olá mundo");
+// }
+// imprimeConsole()
+
+//ex 2
+
+// function imprimeNome(nome) {
+//     console.log(`Olá ${nome}`);
+// }
+// imprimeNome("Maria")
+// imprimeNome("Antonieta")
+// imprimeNome("Felipe")
+
+// function colocarnomeMaiusculo(nome){
+//     const nomeMaiusculo = nome.toUpperCase()
+//     return nomeMaiusculo
+// }
+
+// console.log(colocarnomeMaiusculo("Maria Antonieta"));
+
+function somar(nmr1,nmr2) {
+    const soma = nmr1 + nmr2
+    return soma
+}
+const retornoSoma = somar(10,20)
+console.log(retornoSoma);
